96e5ab016d8cd9ecf548897a72d035233fc7862f
hello-lab <ezioauditoredefinerz@gmail.com>
2025-10-14 23:41:46 +0530

    merge

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 const EventsPage = () => {
   return (
-<<<<<<< HEAD
-    <div className="flex items-center justify-center h-screen  text-white text-4xl font-bold">
-=======
     <div className="flex items-center justify-center h-screen bg-black text-[#F2E6D6] text-4xl font-bold">
->>>>>>> 41257f8a99b37689e8ae74556984f6c87ecbd392
       <h1>Events Page</h1>
     </div>
   );
